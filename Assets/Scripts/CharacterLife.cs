69799f7bd3b3ef13325ac5baec69b73cddd068fc
enzobenedetti <enzob210401@gmail.com>
2022-03-21 18:44:21 +0100

    character Life Working

--- Assets/Scripts/CharacterLife.cs
+++ Assets/Scripts/CharacterLife.cs
@@ -8,20 +8,13 @@ public class CharacterLife : MonoBehaviour
     public int health = 5;
 
     public float bufferTime = 3f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log(other.transform.name);
+        if (other.transform.CompareTag("Fire"))
+        {
+            health--;
+        }
     }
 }
